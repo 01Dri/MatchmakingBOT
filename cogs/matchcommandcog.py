656278b15285db482108fe4f78120559f7c03353
01Dri <drikill01@gmail.com>
2024-03-21 14:49:48 -0300

    Ajustes na votação para cancelar a partida

--- cogs/matchcommandcog.py
+++ cogs/matchcommandcog.py
@@ -27,41 +27,60 @@ class MatchCommandCog(commands.Cog):
         self.message_button = None
         self.result = None
         self.votes = {}
+        self.votes_users = {}
 
     @app_commands.command()
     async def afkcancelar(self, interact: discord.Interaction):
         channel_id = str(interact.channel.category)  # CHANNEL ID É O ID DA MATCH
         match = self.match_service.find_match_by_id(channel_id)
-        if match is not None:
-            if self.votes.values() is not None:
-                self.votes.clear()
-            await interact.response.send_message("Votação para cancelar a partida iniciada")
-            self.message_button = await interact.followup.send("VOTAÇÃO PARA CANCELAR A PARTIDA: ",
-                                                               view=self.button_service.get_view())
+        print(self.message_button)
+        if match is None:
+            await interact.response.send_message("Nenhuma partida encontrada nesse canal", ephemeral=True)
+            return
 
-            await asyncio.sleep(30)
-            if self.get_result_cancel():
-                await interact.followup.send(f"Com {self.votes['sim']} votos em SIM, a partida será encerrada!")
-                await asyncio.sleep(5)
-                await self.remove_attributes_on_match(match)
-                self.update_queue_status_players(match)
-                return
+        if self.message_button is not None:
+            await interact.response.send_message("Já existe uma votação em andamento!", ephemeral=True)
+            return
 
-            print(self.votes)
+        if self.votes.values() is not None:
+            self.votes.clear()
+        await interact.response.send_message("Votação para cancelar a partida iniciada", ephemeral=True)
+        self.message_button = await interact.followup.send("VOTAÇÃO PARA CANCELAR A PARTIDA: ",
+                                                           view=self.button_service.get_view())
+        if "nao" not in self.votes.keys():
+            self.votes["nao"] = 0
+
+        if "sim" not in self.votes.keys():
+            self.votes["sim"] = 0
+
+        await asyncio.sleep(20)
+        if self.votes['sim'] == 0 and self.votes['nao'] == 0:
+            await self.message_button.delete()
+            print("CAIU AQUI")
+            self.message_button = None
             await interact.followup.send(
                 f"Não houve um número suficiente de votos para cancelar a partida, a partida continuará")
+            return
 
+        if self.get_result_cancel(2):
+            await interact.followup.send(f"Com {self.votes['sim']} votos em SIM, a partida será encerrada!")
+            await asyncio.sleep(5)
+            await self.remove_attributes_on_match(match)
+            self.update_queue_status_players(match)
+            self.message_button = None
             return
-        else:
-            await interact.response.send_message("Nenhuma partida encontrada!!!", ephemeral=True)
-            return
+
+        await interact.followup.send(
+            f"Não houve um número suficiente de votos para cancelar a partida, a partida continuará")
+        await self.message_button.delete()
+        self.message_button = None
 
     async def remove_attributes_on_match(self, match: Match):
-        # await match.category.delete()
+        await match.category.delete()
         await match.voice_channel_teams_a.delete()
         await match.voice_channel_teams_b.delete()
         await match.channel_voting_maps.delete()
-        await match.category.delete()
+        # await match.category.delete()
         self.match_service.remove_match(match)
         await match.message_amount_matches.edit(
             embed=embed_queues_message(self.queue_service.get_quantity_players_on_queues(),
@@ -78,42 +97,39 @@ class MatchCommandCog(commands.Cog):
         return
 
     async def callback_vote_cancel_button(self, interact: discord.Interaction):
-        channel_id = str(interact.channel.category)  # CHANNEL ID É O ID DA MATCH
+        channel_id = str(interact.channel.category)
         match = self.match_service.find_match_by_id(channel_id)
         button_id = str(interact.data['custom_id'])
-        await interact.response.send_message(f"Você votou em {button_id}!!!", ephemeral=True)
-        if button_id not in self.votes.keys():
+        if self.add_vote(button_id, interact.user.name) is False:
+            await interact.response.send_message("Você já votou!!!", ephemeral=True)
+            return
+
+        await interact.response.send_message(f"Você votou em {button_id}", ephemeral=True)
+
+    def add_vote(self, button_id, user):
+
+        if user in self.votes_users.keys():
+            return False
+
+        if button_id not in self.votes.keys() and user not in self.votes_users.keys():
             self.votes[button_id] = 1
+            self.votes_users[user] = button_id
+
         else:
             self.votes[button_id] += 1
+            self.votes_users[user] = button_id
 
-        # await asyncio.sleep(30)
-        # if self.get_result_cancel():
-        #     await interact.followup.send(f"Com {self.votes['sim']} votos em SIM, a partida será encerrada!")
-        #     await asyncio.sleep(5)
-        #     await  self.remove_attributes_on_match(match)
-        #     self.update_queue_status_players(match)
-        #     return
-        # print(self.votes)
-        # await interact.followup.send(
-        #     f"Não houve um número suficiente de votos para cancelar a partida, a partida continuará")
+    def get_result_cancel(self, max_votes):
 
-    def get_result_cancel(self):
-        if "nao" not in self.votes.keys():
+        if self.votes["sim"] >= max_votes:
             return True
             # return self.votes["sim"]
-
-        if "sim" not in self.votes.keys():
-            return False
-            # return self.votes["nao"]
-
-        if self.votes["sim"] > self.votes["nao"]:
-            return True
-            # return self.votes["sim"]
-        elif self.votes["nao"] > self.votes["sim"]:
+        elif self.votes["nao"] >= max_votes:
             return False
 
-        return False
+        self.votes.clear()
+        self.votes_users.clear()
+        # return False
 
         # return self.votes["nao"]
 
